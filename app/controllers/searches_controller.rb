de514d41c0919c3033fc0b54266aadffe99c5ae6
EC2 Default User <ec2-user@ip-172-31-22-40.us-east-2.compute.internal>
2019-10-31 22:02:08 +0000

    2

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,17 +2,13 @@ class SearchesController < ApplicationController
     
     def search_params
         params.require(:search).permit(:uin)
-        # debug_inspector(params)
+        
     end
-    
-    
+
     def new
     end
-    
-    
+
     def index
-        @search=[{:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
-            {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
-            {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'}]
+    @search=Search.where(:uin => 327005544)
     end
 end
