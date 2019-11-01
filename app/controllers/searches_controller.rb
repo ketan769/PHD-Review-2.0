8305c841baea3571f3456dcd91996e3503f8ec83
EC2 Default User <ec2-user@ip-172-31-22-40.us-east-2.compute.internal>
2019-11-01 19:26:41 +0000

    -Ketan

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,36 @@
 class SearchesController < ApplicationController
     
+   
     def search_params
-        params.require(:search).permit(:uin)
+        params.require(:search).permit(:uin, :first_name, :last_name, :review_year)
+        # debug_inspector(params)
+    end
+    
+    
+    def new
+    end
+    
+    
+    def index
+        
+        if params[:uin]!=""
+            @search = Search.where(:uin => params[:uin])
+        elsif params[:first_name]!=""
+            @search = Search.where(:first_name => params[:first_name])
+        elsif params[:last_name]!=""
+            @search = Search.where(:last_name => params[:last_name])
+        elsif params[:review_year]!=""
+            @search = Search.where(:review_year=> params[:review_year])
+        else
+            @search = Search.all
+        end
         
     end
-
-    def new
-        @search=[{:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
-            {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
-            {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'}]
-    end
-
-    def index
-    @search=Search.where(:uin => 327005544)
-    end
 end
+
+
+    # def new
+    #     @search=[{:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
+    #         {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
+    #         {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'}]
+    # end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,6 @@
 class Search < ActiveRecord::Base
+ def self.search_func (search)
+    @searches = Search.where(:uin => search[:uin]) #OR :first_name => search[:first_name])
+  end
 end
+
