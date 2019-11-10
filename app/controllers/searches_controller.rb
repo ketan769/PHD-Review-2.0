2d8cfb76c7bfd2abf0d00c5cf184488d00fecf75
EC2 Default User <ec2-user@ip-172-31-22-40.us-east-2.compute.internal>
2019-11-10 18:41:55 +0000

    Index

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,18 +13,21 @@ class SearchesController < ApplicationController
     
     def index
         
-        if params[:uin]!=""
-            @search = Search.where(:uin => params[:uin])
-        elsif params[:first_name]!=""
-            @search = Search.where(:first_name => params[:first_name])
-        elsif params[:last_name]!=""
-            @search = Search.where(:last_name => params[:last_name])
-        elsif params[:review_year]!=""
-            @search = Search.where(:review_year=> params[:review_year])
-        else
-            @search = Search.all
-        end
-        
+        # if params[:uin]!=""
+        #     @search = Search.where(:uin => params[:uin])
+        # elsif params[:first_name]!=""
+        #     @search = Search.where(:first_name => params[:first_name])
+        # elsif params[:last_name]!=""
+        #     @search = Search.where(:last_name => params[:last_name])
+        # elsif params[:review_year]!=""
+        #     @search = Search.where(:review_year=> params[:review_year])
+        # else
+        #     @search = Search.all
+        # end
+        @search=[{:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
+            {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'},
+            {:uin => 327005544, :first_name => 'Ketan', :last_name=> 'sethi'}]
+        logger.debug(@search.inspect)    
     end
 end
 
