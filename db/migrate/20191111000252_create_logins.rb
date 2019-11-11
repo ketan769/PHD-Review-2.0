0223bd04afd9ef0bd205d8a429306269f5aa13d8
harshita-chaudhary <ec2-user@ip-172-31-16-95.us-east-2.compute.internal>
2019-11-11 00:06:00 +0000

    Added login table

---
+++ db/migrate/20191111000252_create_logins.rb
@@ -0,0 +1,12 @@
+class CreateLogins < ActiveRecord::Migration[5.0]
+  def change
+    create_table :logins do |t|
+      t.integer :username
+      t.string, :password
+      t.string, :email
+      t.string :role
+
+      t.timestamps
+    end
+  end
+end
